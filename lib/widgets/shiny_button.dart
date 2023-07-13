08501e732b5b0f496ba3801048620256637461a8
Raouf Rahiche <mrrahicheraouf@gmail.com>
2023-07-13 19:17:09 +0100

    Update shiny_button.dart

--- lib/widgets/shiny_button.dart
+++ lib/widgets/shiny_button.dart
@@ -22,41 +22,43 @@ class _ShinyButtonState extends State<ShinyButton>
   double value = 0;
   @override
   Widget build(BuildContext context) {
-    final child = AnimatedBuilder(
-        animation: animationController,
-        builder: (context, child) {
-          return ShaderBuilder(
-            (context, shader, child) {
-              return AnimatedSampler(
-                (image, size, canvas) {
-                  // Set the values for resolution and iTime
-                  shader.setFloat(0, size.width);
-                  shader.setFloat(1, size.height);
-                  shader.setFloat(2, value);
-                  // shader.setFloat(3, animation2.value);
+    final child = Center(
+      child: AnimatedBuilder(
+          animation: animationController,
+          builder: (context, child) {
+            return ShaderBuilder(
+              (context, shader, child) {
+                return AnimatedSampler(
+                  (image, size, canvas) {
+                    // Set the values for resolution and iTime
+                    shader.setFloat(0, size.width);
+                    shader.setFloat(1, size.height);
+                    shader.setFloat(2, value);
 
-                  shader.setImageSampler(0, image);
-                  value += 0.02;
-                  canvas.drawRect(
-                    Rect.fromLTWH(0, 0, size.width, size.height),
-                    Paint()..shader = shader,
-                  );
-                },
-                child: Center(
-                  child: const Icon(
-                    Icons.add_business_sharp,
-                    size: 200,
+                    shader.setImageSampler(0, image);
+                    value += 0.03;
+                    canvas.drawRect(
+                      Rect.fromLTWH(0, 0, size.width, size.height),
+                      Paint()..shader = shader,
+                    );
+                  },
+                  // child: const Icon(
+                  //   Icons.add_business_sharp,
+                  //   size: 200,
+                  // ),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Click here",
+                      style: TextStyle(fontSize: 40),
+                    ),
                   ),
-                ),
-                // child: TextButton(
-                //   onPressed: () {},
-                //   child: Text("Click here"),
-                // ),
-              );
-            },
-            assetKey: 'shaders/light_sweep.frag',
-          );
-        });
+                );
+              },
+              assetKey: 'shaders/light_sweep.frag',
+            );
+          }),
+    );
     return Scaffold(
       body: child,
     );
